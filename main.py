a6f220990ac5fadc2eb8d5b8aed00aeb192f8b52
Alan Newton <128156285+newtonalan@users.noreply.github.com>
2023-03-29 11:23:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-n = float(input("Digite um numero:\n"))
+n = (input("Digite um numero:\n"))
+print(" ")
 
-n = int(n)
 
-if n < 0:
-  print("Impossivel!")
-elif n < 18:
-  print("O alistamento não é necessário.")
-elif 18 < n <= 65:
-  print("Não deixe de votar na próxima eleição.")
-elif n > 65:
-  print("Vá descansar.")
+if n.isnumeric():
+  n = int(n)
+  if n < 0:
+    print("Impossivel!")
+  elif n < 18:
+    print("O alistamento não é necessário.")
+  elif 18 < n <= 65:
+    print("Não deixe de votar na próxima eleição.")
+  elif n > 65:
+    print("Vá descansar.")
+
 else:
   print("Eita.")
