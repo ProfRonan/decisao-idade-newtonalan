cd9f299f01abf5879e48b16c6ee4ba92d9a395f1
Alan Newton <128156285+newtonalan@users.noreply.github.com>
2023-03-31 10:43:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-n = (input("Digite sua idade atual:\n"))
-print(" ")
+id = int(input("Digite sua idade: \n "))
 
-
-if n.isnumeric():
-  n = int(n)
-  if n < 0:
-    print("Impossivel!")
-  elif n < 18:
-    print("O alistamento não é necessário.")
-  elif 18 <= n <= 65:
-    print("Não deixe de votar na próxima eleição.")
-  elif n > 65:
-    print("Vá descansar.")
-
+if id < 0:
+  print("impossivel!")
+elif 0 < id < 18:
+  print("não precisa se alistar.")
+elif 18 <= id < 65:
+  print("Não esqueça de votar na próxima eleição.")
+elif id > 65:
+  print("Vá descansar.")
 else:
-  print("Eita.")
+  print("eita!")
