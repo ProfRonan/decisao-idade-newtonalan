dce769360285b0dd5022e4fa1ed488a6cda2bc6f
Alan Newton <128156285+newtonalan@users.noreply.github.com>
2023-03-31 10:46:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-id = int(input("Digite sua idade: \n "))
+id = int(input(" "))
 
 if id < 0:
   print("impossivel!")
 elif 0 < id < 18:
   print("não precisa se alistar.")
-elif 18 <= id < 65:
+elif 18 < id < 65:
   print("Não esqueça de votar na próxima eleição.")
 elif id > 65:
   print("Vá descansar.")
