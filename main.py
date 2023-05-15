2fbbccc66458b7b6034311707dba6ebd5f3d4dba
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:06 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 id = int(input(" "))
 
 if id < 0:
-  print("impossível!")
+  print("Impossível!")
 elif 0 < id < 18:
-  print("não precisa se alistar.")
+  print("Não precisa se alistar.")
 elif 18 < id < 65:
   print("Não esqueça de votar na próxima eleição.")
 elif id > 65:
   print("Vá descansar.")
 else:
-  print("eita!")
+  print("Eita!")
